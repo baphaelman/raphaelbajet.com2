533d73cdec5b1508c2a7b5d16ed360ea20b55989
Raphael Bajet <baphael@berkeley.edu>
2025-04-11 22:20:03 -0700

    added WIP message lol

--- vite-project/components/Articles/ArticleOne.jsx
+++ vite-project/components/Articles/ArticleOne.jsx
@@ -53,6 +53,7 @@ function ArticleOne() {
                     <p>A rubik’s cube is made up of 12 edges, 8 corners, and 6 centers, though this article will only look at edges. In this article, ‘edge position’ will be one of the 12 places an edge can be on the cube. An edge’s solved position is the edge position it occupies in a solved cube. An edge will also ‘belong’ in its solved position.</p>
                     <img src="/singleCycles/cube_pieces_diagram.png" alt="Cube Peices Diagram" style={{ width: '70%', margin: '5px 0'}} />
                     <p>Each edge can be moved to any edge position and (for the purposes of this article) any two edges can be swapped at will. If you were crazy enough to try to solve a rubik’s cube blindfolded, this introduces a simple beginner-level strategy, called Old Pochmann: choose an edge and solve it by swapping it with the edge in its solved position.  Now solve that edge by swapping it with the edge in its solved position, on and on until all the edges are solved. If we denote the 12 edges by the numbers (1), (2), …, (12), we can imagine simplifying these swaps to a cycle, as below:</p>
+                    <p>WIP--I'll finish making this page soon!!</p>
                 </div>
             </div>
         </div>
