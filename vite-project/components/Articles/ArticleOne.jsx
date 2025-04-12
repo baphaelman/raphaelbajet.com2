d0f4b7741b87ba819e92e89d6148f5ee947a50a6
Raphael Bajet <baphael@berkeley.edu>
2025-04-11 22:19:22 -0700

    slight single cycles improvements

--- vite-project/components/Articles/ArticleOne.jsx
+++ vite-project/components/Articles/ArticleOne.jsx
@@ -13,7 +13,7 @@ function ArticleOne() {
 
     const contentStyle = {
         padding: "1em",
-        width: '80%',
+        width: '70%',
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
@@ -35,6 +35,9 @@ function ArticleOne() {
     const writingStyle = {
         width: '100%',
         fontSize: '1.25rem',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
     }
 
     return (
@@ -48,31 +51,8 @@ function ArticleOne() {
                 <div style={writingStyle}>
                     <h3 className="nunito-bold">Introduction</h3>
                     <p>A rubik’s cube is made up of 12 edges, 8 corners, and 6 centers, though this article will only look at edges. In this article, ‘edge position’ will be one of the 12 places an edge can be on the cube. An edge’s solved position is the edge position it occupies in a solved cube. An edge will also ‘belong’ in its solved position.</p>
-                    <img src="/singleCycles/cube_pieces_diagram.png" alt="Cube Peices Diagram" style={{ width: '100%', maxWidth: '600px', margin: '1em 0' }} />
-                    <p>More text down here!!</p>
-                    <p>More text down here!!</p>
-                    <p>More text down here!!</p>
-                    <p>More text down here!!</p>
-                    <p>More text down here!!</p>
-                    <p>More text down here!!</p>
-                    <p>More text down here!!</p>
-                    <p>More text down here!!</p>
-                    <p>More text down here!!</p>
-                    <p>More text down here!!</p>
-                    <p>More text down here!!</p>
-                    <p>More text down here!!</p>
-                    <p>More text down here!!</p>
-                    <p>More text down here!!</p>
-                    <p>More text down here!!</p>
-                    <p>More text down here!!</p>
-                    <p>More text down here!!</p>
-                    <p>More text down here!!</p>
-                    <p>More text down here!!</p>
-                    <p>More text down here!!</p>
-                    <p>More text down here!!</p>
-                    <p>More text down here!!</p>
-                    <p>More text down here!!</p>
-                    <p>More text down here!!</p>
+                    <img src="/singleCycles/cube_pieces_diagram.png" alt="Cube Peices Diagram" style={{ width: '70%', margin: '5px 0'}} />
+                    <p>Each edge can be moved to any edge position and (for the purposes of this article) any two edges can be swapped at will. If you were crazy enough to try to solve a rubik’s cube blindfolded, this introduces a simple beginner-level strategy, called Old Pochmann: choose an edge and solve it by swapping it with the edge in its solved position.  Now solve that edge by swapping it with the edge in its solved position, on and on until all the edges are solved. If we denote the 12 edges by the numbers (1), (2), …, (12), we can imagine simplifying these swaps to a cycle, as below:</p>
                 </div>
             </div>
         </div>
